4c69a3fb8fea21ecee66c6babc36946559fb6491
hafij <hafij.alasad23@gmail.com>
2026-02-21 02:35:57 +0600

    feat: hero section add

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,12 +1,12 @@
+import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
 import { Button } from "@mui/material";
 import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
-      <Button>Text</Button>
-    </main>
+    <>
+      <HeroSection />
+    </>
   );
 };
 
