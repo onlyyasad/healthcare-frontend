e773156a51ddc84f4b21f5302933fd1ed0f972d5
hafij <hafij.alasad23@gmail.com>
2026-02-24 22:58:36 +0600

    feat: specialties section added with dynamic data

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/ui/HomePage/HeroSection/HeroSection";
+import Specialist from "@/components/ui/HomePage/Specialist/Specialist";
 import { Button } from "@mui/material";
 import Image from "next/image";
 
@@ -6,6 +7,7 @@ const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <Specialist />
     </>
   );
 };
